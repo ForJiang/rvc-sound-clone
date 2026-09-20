576dcf0ee8c7876509ca7b07e443f4ab999f224a
ForJiang <jianghaoda@users.noreply.github.com>
2026-09-20 12:34:16 +0800

    chore: 回填构建版本号

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,7 @@ let currentCleanup = null;
  * 当前页面构建对应的提交号；与 assets/version.json 比对判断是否有新版。
  * 发版时同步更新这里的值和 assets/version.json 的 version 字段。
  */
-const BUILD = '3827698';
+const BUILD = 'df55d1b';
 
 const CHECK_INTERVAL_MS = 5 * 60 * 1000;   // 5 分钟轮询一次
 let updateNotified = false;
